5b021f11ff524094b4eb6c14484c2b1024adf391
Sebastian Springer <info@sebastian-springer.com>
2017-01-05 20:18:06 +0100

    enemy 3 shape 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const e1 = new Enemy3(ctx, 10, 10);
-e1.drawShape1();
+e1.drawShape2();
 
 //const e2 = new Enemy1(ctx, 150, 10);
 //e2.drawShape2();
