d74af6bd3e7d8050dd18f0e07f0b6a15e9521095
Sebastian Springer <info@sebastian-springer.com>
2017-01-05 16:32:25 +0100

    enemy 2 shape 2

--- src/enemy2.js
+++ src/enemy2.js
@@ -50,6 +50,52 @@ class Enemy2 extends Enemy {
     }
 
     drawShape2() {
+        this.ctx.beginPath();
+        this.moveTo(0, 2);
+        this.lineTo(0, 5);
+        this.lineTo(3, 5);
+        this.lineTo(3, 6);
+        this.lineTo(2, 6);
+        this.lineTo(2, 7);
+        this.lineTo(0, 7);
+        this.lineTo(0, 8);
+        this.lineTo(2, 8);
+        this.lineTo(2, 7);
+        this.lineTo(4, 7);
+        this.lineTo(4, 6);
+        this.lineTo(5, 6);
+        this.lineTo(5, 7);
+        this.lineTo(7, 7);
+        this.lineTo(7, 6);
+        this.lineTo(8, 6);
+        this.lineTo(8, 7);
+        this.lineTo(10, 7);
+        this.lineTo(10, 8);
+        this.lineTo(12, 8);
+        this.lineTo(12, 7);
+        this.lineTo(10, 7);
+        this.lineTo(10, 6);
+        this.lineTo(9, 6);
+        this.lineTo(9, 5);
+        this.lineTo(12, 5);
+        this.lineTo(12, 2);
+        this.lineTo(11, 2);
+        this.lineTo(11, 1);
+        this.lineTo(8, 1);
+        this.lineTo(8, 0);
+        this.lineTo(4, 0);
+        this.lineTo(4, 1);
+        this.lineTo(1, 1);
+        this.lineTo(1, 2);
+        this.ctx.closePath();
+        
+        this.ctx.fillStyle = 'white';
+        this.ctx.fill();
+
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(this.offsetX + this.length * 3, this.offsetY + this.length * 3, this.length * 2, this.length * 1);
+        this.ctx.fillRect(this.offsetX + this.length * 7, this.offsetY + this.length * 3, this.length * 2, this.length * 1);
+        this.ctx.fillRect(this.offsetX + this.length * 5, this.offsetY + this.length * 5, this.length * 2, this.length * 1);
     }
 
 }
