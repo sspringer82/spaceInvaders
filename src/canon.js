5621a4a93f3b11f2d20fef2bce3e2836c0092fe8
Sebastian Springer <info@sebastian-springer.com>
2017-01-06 10:27:12 +0100

    changed canon shape

--- src/canon.js
+++ src/canon.js
@@ -1,24 +1,29 @@
 class Canon extends Enemy {
 
+    constructor(ctx, offsetX = 0, offsetY = 0, length = 10) {
+        super(ctx, offsetX, offsetY, length);
+        this.size = 13 * length;
+    }
+
     draw() {
         this.ctx.beginPath();
 
-        this.moveTo(0, 6);
-        this.lineTo(0, 10);
-        this.lineTo(13, 10);
-        this.lineTo(13, 6);
-        this.lineTo(12, 6);
-        this.lineTo(12, 5);
-        this.lineTo(8, 5);
-        this.lineTo(8, 2);
-        this.lineTo(7, 2);
+        this.moveTo(0, 4);
+        this.lineTo(0, 7);
+        this.lineTo(13, 7);
+        this.lineTo(13, 4);
+        this.lineTo(12, 4);
+        this.lineTo(12, 3);
+        this.lineTo(8, 3);
+        this.lineTo(8, 1);
+        this.lineTo(7, 1);
         this.lineTo(7, 0);
         this.lineTo(6, 0);
-        this.lineTo(6, 2);
-        this.lineTo(5, 2);
-        this.lineTo(5, 5);
-        this.lineTo(1, 5);
-        this.lineTo(1, 6);
+        this.lineTo(6, 1);
+        this.lineTo(5, 1);
+        this.lineTo(5, 3);
+        this.lineTo(1, 3);
+        this.lineTo(1, 4);
         this.ctx.closePath();
 
         this.ctx.fillStyle = 'white';
